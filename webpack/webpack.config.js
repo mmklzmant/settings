0519d13ebcc89766a9975c28dc50331cfd6f35b0
mmklzmant <mmklzmant@163.com>
2017-12-25 20:30:49 +0800

    webpack update

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,25 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const path = require('path');
 
-var webpack = require('webpack');
-var path = require('path');
-
-var BUILD_DIR = path.resolve(__dirname, 'public');
-var APP_DIR = path.resolve(__dirname, 'source');
-
-var config = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel-loader'
-      }
-    ]
-  }
-};
+const config = {
+	entry: './js/src/common_es6.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+	    //将es6代码编译成js
+		loaders:[
+			{
+				test: /\.(js|jsx)$/,
+				use: 'babel-loader'
+			}
+		]
+	},
+	plugins: [
+	    //精简压缩js文件
+		new webpack.optimize.UglifyJsPlugin(),
+		//向html文件里添加script标签引用编译成的bundle.js
+		new HtmlWebpackPlugin({template: './index.html'})
+	],
+	//webpack-dev-server配置npm安装
+    devServer: {
+        contentBase: "./dist", //本地服务器所加载的页面所在的目录
+        historyApiFallback: true, //不跳转
+        inline: true,
+        port: 4200
+    },
+}
 
 module.exports = config;
